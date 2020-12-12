1885da370b7c4db7fa616cc9bf62c15b41be7e2e
sbarnesthornton <sepbt@btinternet.com>
2020-12-12 13:04:33 +0000

    Added stopwatch and stop/start button
    Also added a test to check the controller is up and running

--- engine/src/main/resources/static/js/sketch.js
+++ engine/src/main/resources/static/js/sketch.js
@@ -2,26 +2,40 @@ const SIZE = 400;
 const x = SIZE / 2;
 const y = SIZE / 2;
 const r = SIZE / 5;
-var speeds = ["1400","1500","1600", "1700", "1800", "1900", "2000","2100","2200","2300","2400", "----"];
-var id = ["480865808", "480866073", "480866431", "480866534", "484084134", "484084340", "484084647", "484084941", "", "", "", ""];
-var count = speeds.length - 1;
-var w = window.innerWidth;
-var h = window.innerHeight;
+let speeds = ["1400","1500","1600", "1700", "1800", "1900", "2000","2100","2200","2300","2400"];
+let id = ["480865808", "480866073", "480866431", "480866534", "484084134", "484084340", "484084647", "484084941", "", "", "", ""];
+let count = 0;
+let w = window.innerWidth;
+let h = window.innerHeight;
+let paused = true;
 let myFont;
-document.getElementsByTagName("BODY")[0].onresize = function() {myFunction()};
+let time = 0;
 
 function setup() {
   let w = window.outerWidth;
   let h = window.outerHeight;
-  let cnv = createCanvas(w/2, h/2);
-  cnv.position(w/2,0);
-  /*button = createButton('play');
-  button.position(x, y + r + 40);
-  button.mousePressed(play);*/
+  let cnv = createCanvas(w - 640, 360);
+  cnv.position(640,0);
+  let button = createButton('Start/Stop');
+  let col = color(25, 23, 200, 50);
+  button.style('background-color', col)
+  button.style('font-size', '24px')
+  button.position(640 + SIZE, y - button.height);
+  button.mousePressed(playPause);
 }
 
 function preload() {
   myFont = loadFont('js/digital-7.ttf');
+}
+
+function playPause() {
+  if (paused) {
+    paused = false;
+    player.play();
+  }else {
+    paused = true;
+    player.pause();
+  }
 }
 
 function draw() {
@@ -32,27 +46,52 @@ function draw() {
   ellipse(0,0,2*r,2*r);
   pop();
   push();
-  fill('black')
+  fill('black');
   translate(x,y);
   ellipse(0,0,r,r);
   pop();
   push();
   fill(0);
-  rect(x - (SIZE / 10), y - r -((SIZE/10)*1.3),(SIZE / 10) * 2,(SIZE / 10))
+  ellipse(x + (SIZE*1.325),y, 2*r, 2*r);
   pop();
+  push();
+  fill('white');
+  ellipse(x + (SIZE*1.325), y, 2*r - 10, 2*r - 10);
+  pop();
+  push();
   fill('red');
   textAlign(CENTER);
   textSize(SIZE / 10);
   textFont(myFont);
-  text(speeds[count], x, y - r - 20)
+  text(time, x + (SIZE*1.325), y);
+  pop();
+  push();
+  fill(0);
+  rect(x - (SIZE / 10), y - r -((SIZE/10)*1.3),(SIZE / 10) * 2,(SIZE / 10));
+  pop();
+  fill('red');
+  push();
+  textAlign(CENTER);
+  textSize(SIZE / 10);
+  textFont(myFont);
+  text(speeds[count], x, y - r - 20);
+  pop();
   push();
   stroke('black');
   strokeWeight(2);
   var angle = ((2*PI) / speeds.length) * (count + 1);
   line(x, y, (x + (r * sin(angle))), (y - (r *cos(angle))));
   pop();
+  push();
+  fill(0);
+  rect(x + (SIZE*1.325) + r, y -10, 10, 20);
+  pop();
 }
 
+var timeChange = function(data) {
+  time = data.seconds;
+  redraw();
+}
 
 function mousePressed() {
   if (dist(mouseX,mouseY, x, y) < r) {
@@ -60,9 +99,7 @@ function mousePressed() {
     if (count > (speeds.length -1) ){
       count = 0;
     }
-  var video = document.getElementById("video");
-    video.src = "https://player.vimeo.com/video/".concat(id[count]);
-    video.contentDocument.location.reload(true);
+  player.loadVideo(id[count])
 
   }
 }
--- engine/src/test/java/com/uob/engine/EngineApplicationTests.java
+++ engine/src/test/java/com/uob/engine/EngineApplicationTests.java
@@ -1,13 +1,18 @@
 package com.uob.engine;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EngineApplicationTests {
 
+	@Autowired WebController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
