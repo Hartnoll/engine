18c5e15237118e3ad1fa88088c33d836870558a9
sbarnesthornton <56411235+sbarnesthornton@users.noreply.github.com>
2020-12-14 19:44:29 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ echo "Generating jar file"
 
 #Copy execute_commands_on_ec2.sh file which has commands to be executed on server... Here we are copying this file
 # every time to automate this process through 'deploy.sh' so that whenever that file changes, it's taken care of
-scp -i "~/Downloads/aws-demo.pem" execute_commands_on_ec2.sh ec2-user@ec2-18-225-9-2.us-east-2.compute.amazonaws.com:/home/ec2-user
+scp execute_commands_on_ec2.sh ubuntu@100.26.152.21:/home/ubuntu
 echo "Copied latest 'execute_commands_on_ec2.sh' file from local machine to ec2 instance"
 
-scp -i "~/Downloads/aws-demo.pem" build/libs/demo-0.0.1-SNAPSHOT.jar ec2-user@ec2-18-225-9-2.us-east-2.compute.amazonaws.com:/home/ec2-user
+scp target/demo-0.0.1-SNAPSHOT.jar eubuntu@100.26.152.21:/home/ubuntu
 echo "Copied jar file from local machine to ec2 instance"
 
 echo "Connecting to ec2 instance and starting server using java -jar command"
-ssh -i "~/Downloads/aws-demo.pem" ec2-user@ec2-18-225-9-2.us-east-2.compute.amazonaws.com ./execute_commands_on_ec2.sh
+ssh ubuntu@100.26.152.21:/home/ubuntu ./execute_commands_on_ec2.sh
