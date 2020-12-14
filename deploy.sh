77bb671547a5b4e70a6db861ec7c82d2c6afa1ea
sbarnesthornton <56411235+sbarnesthornton@users.noreply.github.com>
2020-12-14 19:45:42 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ echo "Generating jar file"
 scp execute_commands_on_ec2.sh ubuntu@100.26.152.21:/home/ubuntu
 echo "Copied latest 'execute_commands_on_ec2.sh' file from local machine to ec2 instance"
 
-scp target/demo-0.0.1-SNAPSHOT.jar eubuntu@100.26.152.21:/home/ubuntu
+scp engine/target/engine-0.0.1-SNAPSHOT.jar eubuntu@100.26.152.21:/home/ubuntu
 echo "Copied jar file from local machine to ec2 instance"
 
 echo "Connecting to ec2 instance and starting server using java -jar command"
